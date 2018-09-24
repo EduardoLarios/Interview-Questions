81f1b975bf8269cf27f02b45665d6c943d9d5457
Eduardo Larios <eduardo.larios.fdz@outlook.com>
2018-09-23 22:59:57 -0500

    Just printing the matrix on main

--- CodeFights/Program.cs
+++ CodeFights/Program.cs
@@ -7,7 +7,18 @@ namespace CodeFights {
 
             var input = new int[][] { new int[] { 10,9,6,3,7 }, new int[] { 6, 10, 2, 9, 7 }, new int[] { 7, 6, 3, 8, 2 }, new int[] { 8, 9, 7, 9, 9 }, new int[] { 6, 8, 6, 8, 2 } };
             var ans = Methods.RotateImage(input);
-            Console.ReadLine ();
+
+            for (int i = 0; i < ans.Length; i++)
+            {
+                for (int j = 0; j < ans.Length; j++)
+                {
+                    Console.Write(ans[i][j] + "\t");
+                }
+
+                System.Console.WriteLine();
+            }
+            
+            Console.ReadLine();
         }
     }
 }
